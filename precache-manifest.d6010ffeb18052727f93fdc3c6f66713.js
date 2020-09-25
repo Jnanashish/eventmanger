3b43f7361ffcea1605947efd48ca5f0c972ec125
Jnanashish Handique <jhandique1999@gmail.com>
2020-09-25 08:34:01 +0530

    Updates

---
+++ precache-manifest.d6010ffeb18052727f93fdc3c6f66713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9575716712f0012494950b5b5871cd96",
+    "url": "/eventmanager/index.html"
+  },
+  {
+    "revision": "a18cb95a13f6f601c5d5",
+    "url": "/eventmanager/static/js/2.5425c51c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eventmanager/static/js/2.5425c51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70dbcfe548cadcc187c2",
+    "url": "/eventmanager/static/js/main.72482292.chunk.js"
+  },
+  {
+    "revision": "d504bfe31a9dfc253d42",
+    "url": "/eventmanager/static/js/runtime-main.fd92ac3d.js"
+  }
+]);
